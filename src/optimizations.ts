8e89882821d830fba03d821ca2b526d9f649380b
qsctr <brettonchen@gmail.com>
2016-11-28 07:20:29 +0800

    Add more unfinished stuff

--- src/optimizations.ts
+++ src/optimizations.ts
@@ -1,59 +1,62 @@
 import { Node, BlockStatement } from 'estree';
+import { loopChildren } from './utils';
 
-export type Optimization = (node: Node) => [Node, string] | null;
+export type Optimization = (node: Node) => boolean;
 
 export const optimizations: { [name: string]: Optimization } = {
 
-    emptyStatements: (node: Node) => {
-        const block = getBlock(node);
-        if (block) {
-            const index = block.findIndex(stmt => stmt.type === 'EmptyStatement');
-            if (index >= 0) {
-                return [remove(block, index), 'Empty statement, removed'];
-            }
-        }
-        return null;
-    },
+    emptyStatements: removeFromBlockIf(
+        stmt => stmt.type === 'EmptyStatement',
+        'Empty statement, removed'
+    ),
 
-    uselessStatements: (node: Node) => {
-        const block = getBlock(node);
-        if (block) {
-            for (let i = 0; i < block.length; i++) {
-                const stmt = block[i];
-                if (stmt.type === 'ExpressionStatement') {
-                    switch (stmt.expression.type) {
-                        case 'ThisExpression':
-                            return [remove(block, i), 'Useless this, removed'];
-                        case 'Literal':
-                            if (stmt.expression.value === 'use strict') {
-                                continue;
-                            }
-                            return [remove(block, i), 'Useless literal, removed'];
-                        case 'Identifier':
-                            return [remove(block, i), 'Useless identifier, removed'];
-                    }
-                }
-            }
-        }
-        return null;
-    },
+    uselessStatements: removeFromBlockIf(
+        stmt => stmt.type === 'ExpressionStatement' && isPureExpression(stmt.expression),
+        'Useless statement, removed'
+    ),
 
     uselessReturn: (node: Node) => {
         const block = getFunctionBlock(node);
         if (block) {
             const last = block[block.length - 1];
             if (last.type === 'ReturnStatement' && (!last.argument
-            || (last.argument.type === 'Identifier' && last.argument.name === 'undefined')))
-            // return [remove(block, -1), 'Useless return, removed'];
+            || (last.argument.type === 'Identifier' && last.argument.name === 'undefined'))) {
+                return [remove(block, -1), 'Useless return, removed'];
+            }
         }
-        return null;
+        return true;
     },
 
     voidOperator: (node: Node) => {
-        
+
+        if (node.type === 'ExpressionStatement'
+        && node.expression.type === 'UnaryExpression'
+        && node.expression.operator === 'void'
+        && isPureExpression(node.expression.argument)) {
+            node.expression = {
+                type: 'Identifier',
+                name: 'undefined'
+            };
+            return 
+        }
+    },
+
+    invalidReturn: (node: Node) => {
+        if (isBlockFunction(node)) {
+            return false;
+        }
+        const block = getBlock(node);
+        if (block) {
+            for (const stmt of block) {
+                if (stmt.type === 'ReturnStatement') {
+                    return [stmt, 'Invalid return statement'];
+                }
+            }
+        }
+        return true;
     }
 
-}
+};
 
 function getBlock(node: Node) {
     switch (node.type) {
@@ -66,15 +69,58 @@ function getBlock(node: Node) {
     return null;
 }
 
+function removeFromBlockIf(pred: (node: Node) => boolean, message: string): Optimization {
+    return node => {
+        const block = getBlock(node);
+        if (block) {
+            for (let i = 0; i < block.length;) {
+                if (pred(block[i])) {
+                    print(remove(block, i), message);
+                } else {
+                    i++;
+                }
+            }
+        }
+        return true;
+    };
+}
+
+function replaceChildIf(pred: (node: Node) => boolean, newChild: Node, message: string): Optimization {
+    return node => {
+        loopChildren(node, child => {
+            if (pred(child)) {
+                return newChild;
+            }
+        });
+        return true;
+    }
+}
+
+function isBlockFunction(node: Node) {
+    return node.type === 'FunctionDeclaration'
+        || node.type === 'FunctionExpression'
+        || (node.type === 'ArrowFunctionExpression' && node.body.type === 'BlockStatement');
+}
+
 function getFunctionBlock(node: Node) {
-    if (node.type === 'FunctionDeclaration'
-    || node.type === 'FunctionExpression'
-    || (node.type === 'ArrowFunctionExpression' && node.body.type === 'BlockStatement')) {
-        return (node.body as BlockStatement).body;
+    if (isBlockFunction(node)) {
+        return (node as { body: BlockStatement }).body.body;
     }
     return null;
 }
 
+function isPureExpression(node: Node) {
+    return ['ThisExpression', 'Literal', 'Identifier'].includes(node.type);
+}
+
 function remove(arr: Node[], i: number) {
     return arr.splice(i, 1)[0];
+}
+
+function print({ loc }: Node, message: string) {
+    if (loc) {
+        console.log(`line ${loc.start.line} col ${loc.start.column} to line ${loc.end.line} col ${loc.end.column}: ${message}`);
+    } else {
+        console.log(`<no location info>: ${message}`);
+    }
 }
--- src/search.ts
+++ src/search.ts
@@ -1,36 +1,10 @@
-import { Syntax } from 'esprima';
-import { Node } from 'estree';
+import { Node, Program } from 'estree';
 import { Optimization } from './optimizations';
+import { loopChildren } from './utils';
 
-export default (node: Node, optimize: Optimization) =>
+export default (program: Program, optimize: Optimization) =>
     (function search(node: Node) {
-        while (true) {
-            const result = optimize(node);
-            if (!result) break;
-            const [{ loc }, message] = result;
-            if (!loc) continue;
-            console.log(`line ${loc.start.line} col ${loc.start.column} to line ${loc.end.line} col ${loc.end.column}: ${message}`);
+        if (optimize(node)) {
+            loopChildren(node, search);
         }
-        for (const property in node) {
-            if (node.hasOwnProperty(property)) {
-                const child = node[property];
-                if (isNode(child)) {
-                    search(child);
-                }
-                if (Array.isArray(child) && child.length && isNode(child[0])) {
-                    child.forEach(search);
-                }
-            }
-        }
-    })(node);
-
-function isNode(object: Object) {
-    if (object.hasOwnProperty('type')) {
-        for (const type in Syntax) {
-            if ((object as { type: string }).type === type) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
+    })(program);
